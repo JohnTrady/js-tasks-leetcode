10cd39a27872fb449817f9a88259473ba507282f
John Trady <docktor.mom19.93@gmail.com>
2023-01-07 19:06:35 +0300

    Task-7 (26. Remove Duplicates from Sorted Array from leetcode.com)

--- js/script.js
+++ js/script.js
@@ -2,23 +2,45 @@
 
 
 
-// Task-7
+// Task-7 (26. Remove Duplicates from Sorted Array from leetcode.com)
 
+/**
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 
 const  removeDuplicates = nums => {
-   let result= [];
-   for(let i = 0; i < nums.length; i++) {
-      if(nums[i] !== nums[i + 1]) {
-         result.push(nums[i]);
+   let  uniqElements = 1;
+   for(let i = 1; i < nums.length; i++) {
+      if(nums[i]  !== nums[i - 1]) {
+        nums[uniqElements] = nums[i];
+        uniqElements++;
+
       }
    }
-   return result;
+   
+ 
+   return uniqElements;
 }
 
 
 
  console.log(removeDuplicates([1, 1, 2]));
  console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+
+/**
+ * Runtime 82 ms
+ * Beats 82.67%
+ */
+
+/**
+ * Memory 45.1 MB
+ * Beats 17.81%
+ */
 
 
 
