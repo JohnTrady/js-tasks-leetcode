e09455df00aabe53d20024fdc1e7a71869ffdfc7
John Trady <docktor.mom19.93@gmail.com>
2023-01-15 21:55:29 +0300

    Task-14 (70. Climbing Stairsfrom lettcode.com)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 'use strict';
-// Task- (... from lettcode.com)
+// Task-14 (70. Climbing Stairsfrom lettcode.com)
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+const climbStairs = n => {
+   let pre = 1;
+   let curr = 1;
+   let temp;
+   for(let i = 1; i < n; i++) {
+      temp = curr;
+      curr = curr +  pre;
+      pre = temp;
+   }
+    return curr;
+}; 
+
+    console.log(climbStairs(1));
+    console.log(climbStairs(2));
+    console.log(climbStairs(3));
+    console.log(climbStairs(4));
+    console.log(climbStairs(5));
 
 
 
 /**
- * Runtime 
-   Beats 
+ * Runtime 61 ms
+   Beats 85.81% 
  */
 
 /**
- * Memory 
-   Beats 
+ * Memory 41.3 MB 
+   Beats 97.29%
  */
