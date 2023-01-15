2d3e3b7e0da2d7525c00c4837b5a47aabaf9ebc6
John Trady <docktor.mom19.93@gmail.com>
2023-01-15 20:40:55 +0300

    Task-13 (69. Sqrt(x) from lettcode.com)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
 'use strict';
-// Task- (... from lettcode.com)
-
-
+// Task-13 (69. Sqrt(x) from lettcode.com)
 
 /**
- * Runtime 
-   Beats 
+ * Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
  */
 
 /**
- * Memory 
-   Beats 
+ * @param {number} x
+ * @return {number}
+ */
+
+ const mySqrt = x => {
+    if (x < 0) x = -x;
+    if(x === 0 || x === 1) return x;
+   for(let i = 2; i <= x; i++) {
+        if(i * i === x) {
+            return  i;
+        } else if (i * i > x) {
+            return i - 1;
+        }
+        
+   }
+};
+
+console.log(mySqrt(4));
+console.log(mySqrt(8));
+console.log(mySqrt(-16));
+console.log(mySqrt(1));
+console.log(mySqrt(2));
+
+
+
+/**
+ * Runtime 77 ms
+   Beats 85.11%
+ */
+
+/**
+ * Memory 42.8 MB
+   Beats  76.56%
  */
