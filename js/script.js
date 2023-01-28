7b2857a0bda5205de7f6567666b8495e32eed538
John Trady <docktor.mom19.93@gmail.com>
2023-01-28 22:53:04 +0300

    Task-17 (2. Add Two Numbers from leetcode.com)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-// Task-17 (2. Add Two Numbers from lettcode.com)
+// Task-17 (2. Add Two Numbers from leetcode.com)
 
 /**
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
